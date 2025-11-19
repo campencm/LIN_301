f5180b4254dcdbb756b17d07704a72bf50d82389
CJ Campen <campencm@gmail.com>
2025-11-19 12:10:26 -0500

    Wed, Nov 19th, 2025 at 12:10pm

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash                 
+echo "This is my first script."
+curl -o term_comm.txt https://winjapati.github.io/301_Fall_2025/3_command_line/12_term_comm.txt#
+mkdir folder_1
+mv term_comm.txt folder_1
+echo term_comm.txt
